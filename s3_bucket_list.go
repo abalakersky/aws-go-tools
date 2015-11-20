8219903662f59a455ed6a95e7a039d857397c86b
Alex Balakersky <alex.balakersky@dlt.com>
2015-11-20 14:05:53 -0500

    adding godep

--- s3_bucket_list.go
+++ s3_bucket_list.go
@@ -5,22 +5,22 @@ import (
 	"log"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"my_tools/Godeps/_workspace/src/github.com/aws/aws-sdk-go/aws"
+	"my_tools/Godeps/_workspace/src/github.com/aws/aws-sdk-go/aws/session"
+	"my_tools/Godeps/_workspace/src/github.com/aws/aws-sdk-go/service/s3"
 )
 
 func main() {
 	t := time.Now()
 	svc := s3.New(session.New(&aws.Config{Region: aws.String("us-east-1")}))
-	result,  err := svc.ListBuckets(&s3.ListBucketsInput{})
+	result, err := svc.ListBuckets(&s3.ListBucketsInput{})
 	if err != nil {
 		log.Println("Failed to list buckets", err)
 		return
 	}
 
 	fmt.Println("Here are your buckets on", t.Format(time.RFC1123), "\n")
-//	fmt.Println(result)
+	//	fmt.Println(result)
 	for _, bucket := range result.Buckets {
 		fmt.Printf("%s\n", *bucket.Name)
 	}
--- s3_object_list.go
+++ s3_object_list.go
@@ -3,30 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
-//	"os"
-	"time"
+	//	"os"
 	"flag"
+	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"my_tools/Godeps/_workspace/src/github.com/aws/aws-sdk-go/aws"
+	"my_tools/Godeps/_workspace/src/github.com/aws/aws-sdk-go/aws/session"
+	"my_tools/Godeps/_workspace/src/github.com/aws/aws-sdk-go/service/s3"
 )
 
 func main() {
 	t := time.Now()
 	bucket := flag.String("bucket", "test", "Bucket Name to list objects from")
 	flag.Parse()
-//	fmt.Printf("Your bucket name is %q", *bucket)
+	//	fmt.Printf("Your bucket name is %q", *bucket)
 
 	svc := s3.New(session.New(&aws.Config{Region: aws.String("us-east-1")}))
-	result,  err := svc.ListObjects(&s3.ListObjectsInput{Bucket: bucket})
+	result, err := svc.ListObjects(&s3.ListObjectsInput{Bucket: bucket})
 	if err != nil {
 		log.Println("Failed to list objects", err)
 		return
 	}
 
 	fmt.Printf("Here are the objects in %q bucket on %s\n\n", *bucket, t.Format(time.RFC1123))
-//	fmt.Println(result)
+	//	fmt.Println(result)
 	for _, object := range result.Contents {
 		fmt.Printf("%s\n", *object.Key)
 	}
